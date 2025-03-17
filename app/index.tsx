f725f948e1985a17d4d7bf0581f27dd95c73d0e7
Pete <opletaevpetr@mail.ru>
2025-03-18 02:22:17 +0300

    Descrease Gap

--- app/index.tsx
+++ app/index.tsx
@@ -99,7 +99,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    gap: 24,
+    gap: 20,
   },
   imageContainer: {
     width: 100,
